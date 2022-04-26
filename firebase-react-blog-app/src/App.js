c0b5c6fcf6438750e3de8b8b70b3e23f9eecc34a
Tomasz Rohda <tomaszrohda@wp.pl>
2022-04-26 09:25:53 +0200

    Firebase react blog app -  Form and "creating user in db functionality" added  in Home component

--- firebase-react-blog-app/src/App.js
+++ firebase-react-blog-app/src/App.js
@@ -4,7 +4,7 @@ import Home from "./Home";
 function App() {
   return (
     <div>
-      <Blogs />
+      <Home />
     </div>
   );
 }
--- firebase-react-blog-app/src/Home.js
+++ firebase-react-blog-app/src/Home.js
@@ -1,7 +1,48 @@
 import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./db";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handlerLogIn = () => {};
+  const handlerRegister = async () => {
+    const response = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = response.user;
+
+    console.log(user);
+  };
+
+  return (
+    <>
+      <form>
+        <label htmlFor="email">E-mail: </label>
+        <input
+          id="email"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <label htmlFor="pswd">Password: </label>
+        <input
+          id="pswd"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button onClick={handlerLogIn}>Log In</button>
+        <button onClick={handlerRegister}>Register</button>
+      </form>
+    </>
+  );
 };
 
 export default Home;
